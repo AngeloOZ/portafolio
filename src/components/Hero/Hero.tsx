54582da59dd309155b5ac952def7b43609008e38
angelooz <angelo-mjz7@hotmail.com>
2023-08-08 09:18:34 -0500

    feat: cargar la info

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { HeroParticles } from './HeroParticles';
+
 
 import css from './styles/Hero.module.css'
-import { HeroText, Navbar, HeroAvatar } from '.';
 
+import { HeroText, Navbar, HeroAvatar, HeroParticles } from '.';
 
-export const Hero = () => {
+import { Hero as Herotype } from '@/interfaces';
+
+interface HeroProps {
+  readonly heroData: Herotype;
+}
+export const Hero = ({ heroData }: HeroProps) => {
   return (
     <header className={css.hero}>
       <Navbar />
       <HeroParticles className={css.hero__particles} />
-      <HeroText className={css.hero__text}  />  
-      <HeroAvatar className={css.hero__avatar} classNameImage={css.hero__avatar__img}/>
+      <HeroText className={css.hero__text} />
+      <HeroAvatar className={css.hero__avatar} classNameImage={css.hero__avatar__img} />
     </header>
   )
 }
--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -17,7 +17,6 @@ export const HeroText = ({ ...props }: Props) => {
             </h1>
             <TypeAnimation
                 preRenderFirstString={true}
-
                 sequence={[
                     500,
                     'I am Frontend Developer', // initially rendered starting point
@@ -29,6 +28,7 @@ export const HeroText = ({ ...props }: Props) => {
                 className={css.hero__text__typing}
                 repeat={Infinity}
             />
+            <p className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">About Me</p>
         </div>
     )
 }
