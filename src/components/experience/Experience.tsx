b5213794470b92f9c00270934d19cc788342dc29
angelooz <angelo-mjz7@hotmail.com>
2023-08-21 06:29:55 -0500

    feat experidnce

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,44 +1,43 @@
 import React from 'react'
 
+import { BiSolidBriefcase } from 'react-icons/bi';
+import { FaFlagCheckered } from 'react-icons/fa';
+
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { useContentContex } from '@/context/Content';
 
 import css from './styles/experience.module.scss'
-import { Icon } from './icon';
 
 export const Experience = () => {
+  const { experience } = useContentContex();
   return (
     <div className={css.contenedor}>
-      <VerticalTimeline layout='1-column-left' lineColor='red'>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<Icon />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<Icon />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-
+      <h1>{experience.title}</h1>
+      <VerticalTimeline layout='1-column-left' lineColor='#000'>
+        {
+          experience.jobs.map((job, index) => (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+              date={job.period}
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              icon={<BiSolidBriefcase size={15} />}
+              key={job.company + index}
+            >
+              <h3 className="vertical-timeline-element-title">{job.company}</h3>
+              <h4 className="vertical-timeline-element-subtitle">{job.position}</h4>
+              {
+                job?.description && (
+                  <p>{job.description}</p>
+                )
+              }
+            </VerticalTimelineElement>
+          ))
+        }
         <VerticalTimelineElement
           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          icon={<Icon />}
+          icon={<FaFlagCheckered size={18} />}
         />
       </VerticalTimeline>
     </div>
